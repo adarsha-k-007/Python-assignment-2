b85e8c9870f06e364aaebfb3075db480cda97b53
adarsha-k-007 <adk22aiml@cmrit.ac.in>
2026-02-24 08:45:48 +0530

    Refactor grade calculator for improved clarity

--- _6_grade_calculator.py
+++ _6_grade_calculator.py
@@ -2,42 +2,49 @@ print("============GRADE CALCULATOR===========")
 print()
 print("Enter the obtained Marks out of 100: ")
 print()
-marks = []
-for i in range(1,6):
+
+total = 0
+pas = True   # To check all subjects >= 40
+
+# Taking marks input
+for i in range(1, 6):
     n = int(input(f"Enter marks for subject {i} : "))
-    marks.append(n)
 
-total = sum(marks)
-percentage = (total/500)*100
+    total = total + n
 
-#grade
+    if n < 40:
+        pas = False
+
+percentage = (total / 500) * 100
+
+
+# Grade Calculation
 if percentage >= 90:
     grade = "A+"
 elif percentage >= 80:
     grade = "A"
 elif percentage >= 70:
-    grade = "B" 
+    grade = "B"
 elif percentage >= 60:
-    grade = "C" 
+    grade = "C"
 elif percentage >= 50:
     grade = "D"
-elif percentage < 50:
-    grade = "F(fail)"  
+else:
+    grade = "F (Fail)"
 
-#result
-if all(n >= 40 for n in marks):
+
+# Result Calculation
+if pas:
     result = "PASS"
 else:
     result = "FAIL"
 
-# output
+
+# Output
 print("\n__________MARKS DETAILS_________")
-for i in range(5):
-    print(f"Subject {i+1} : {marks[i]}")
-# print("\nMarks    : ",marks)
 print("--------------------------------")
-print("Total      : ",total,"/500 ")
-print("Percentage : ",percentage)
-print("Grade      : ",grade)
-print("Result     : ",result)
+print("Total      : ", total, "/500")
+print("Percentage : ", percentage)
+print("Grade      : ", grade)
+print("Result     : ", result)
 print("________________________________")
